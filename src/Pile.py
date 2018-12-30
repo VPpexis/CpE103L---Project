8a2020a8a31aef3fdab9b7356d5409c14e5a2970
Mothy20 <45852635+Mothy20@users.noreply.github.com>
2018-12-30 15:34:57 +0800

    Add files via upload

--- src/Pile.py
+++ src/Pile.py
@@ -1,14 +1,41 @@
 from src.Card import Card
 
 class Pile:
-	
-	pile = [Card()]
-	
-	def getSize(self):
-		return "s"
-	
-	def clear(self):
-		
-	def addCard(self):
-		
-	
+        pile=[Card()]
+        front=1
+        end=4
+        card=Card()
+        
+
+        def clear(self):
+                global front,end
+                front=0
+                end=0
+                print("front",front,"end",end)
+
+        def getSize(self):
+                global front,end
+                pileSize=end-front
+                return pileSize
+
+        def addCard(self):
+                global end,pile,card
+                pile[end]=card
+                end+=1
+
+        def addCards(self):
+                while self.getSize()>0:
+                        addCard(self.nextCard())
+
+        def nextCard(self):
+                global front,pile,card
+                if front==end:
+                        return "No cards Left boi."
+                else:
+                        pile[front]=card
+                        front+=1
+                        return c
+        
+        
+
+        
